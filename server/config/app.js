e9ad6ce3260e4e26b7d69a01bbaad7e420a85dcb
Roshan <rrai4959@gmail.com>
2022-12-13 09:40:39 -0500

    fixing eerrorrrr

--- server/config/app.js
+++ server/config/app.js
@@ -7,8 +7,13 @@ let logger = require('morgan');
 let cors = require('cors');
 
 // modules for authentication
-let session = require('express-session');
+// let session = require('express-session');
+let session = require('cookie-session');
 let passport = require('passport');
+
+//var session = require('express-session');
+
+// to var session = require('cookie-session');
 
 let passportJWT = require('passport-jwt');
 let JWTStrategy = passportJWT.Strategy;
@@ -49,7 +54,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use(cors());
-
+app.set('trust proxy', 1);
 //setup express session
 app.use(session({
   secret: "SomeSecret",
@@ -109,14 +114,21 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', { title: 'Error'});
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error', { title: 'Error'});
+// });
+
+app.use(function(req,res,next){
+  if(!req.session){
+      return next(new Error('Oh no')) //handle error
+  }
+  next() //otherwise continue
+  }); 
 
 module.exports = app;
